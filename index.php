6d4f6466729b2e7d5d9d5bd3ed4648d3223fa10d
jgordijo <47391851+jgordijo@users.noreply.github.com>
2019-10-24 23:44:59 -0300

    Site pronto.
    Site feito durante o curso de Wordpress, desenvolvido em HTML e CSS sem frameworks.

---
+++ index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Danki Code - Tema</title>
+	<meta charset="utf-8">
+	<link rel="icon" href="https://cursos.dankicode.com/app/Views/public/favicon.ico" type="image/x-icon" />
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+	<link href="style.css" rel="stylesheet">
+</head>
+<body>
+
+	<section class="topo">
+		<div class="center">
+			<header>
+				<div class="logo"><img src="images/logo.png" /></div><!--logo-->
+			</header>
+			<ul class="menu-desktop">
+				<li><a href="">Home</a></li>
+				<li><a href="">Sobre</a></li>
+				<li><a href="">Contato</a></li>
+			</ul>
+			<div class="menu-mobile">
+			<i class="fas fa-align-right"></i>
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Sobre</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+			</div><!--menu-mobile-->
+			<div class="clear"></div>
+			<br />
+			<br />
+			<div class="w50 time-descricao">
+				<h2>Melhore a comunicação com seu cliente e time.</h2>
+				<p>Consultoria especializada em startups, empresas, principalmente, pessoas.</p>
+				<a target="_blank" href="https://dankicode.com">Ver demonstração</a>
+			</div><!--w50-->
+			<div class="w50 time-imagem">
+				<img src="images/equipe.png" />
+			</div><!--w50-->
+			<div class="clear"></div>
+		</div><!--center-->
+	</section><!--topo-->
+	<div class="circle"><i class="fas fa-angle-down"></i></div>
+
+	<section class="clientes-slider">
+
+		<div class="center">
+		<div style="max-width: 700px;margin: 0 auto;overflow: hidden;" class="slider-container">
+			<img src="images/amazon.jpg" />
+			<img src="images/costco.jpg" />
+			<img src="images/dominos.jpg" />
+			<img src="images/uber.jpg" />
+			<img src="images/walmart.jpg" />
+			<img src="images/amazon.jpg" />
+			<img src="images/costco.jpg" />
+			<img src="images/dominos.jpg" />
+			<img src="images/uber.jpg" />
+			<img src="images/walmart.jpg" />
+		</div>
+		</div><!--center-->
+	</section>
+
+
+	<section class="diferenciais">
+			
+			<div class="center">
+				<h2>Contribuímos de ponta a ponta</h2>
+
+				<div class="icons-diferenciais">
+
+					<div class="box-single-diferenciais">
+						<img src="images/icon1.png" />
+						<h3>Ambientes Mobile</h3>
+						<p>Garanta que toda sua comunicação esteja alinhada com seu propósito, cada palavra conta.</p>
+					</div><!--box-single-diferenciais-->
+
+					<div class="box-single-diferenciais">
+						<img src="images/icon2.png" />
+						<h3>Ambientes Mobile</h3>
+						<p>Garanta que toda sua comunicação esteja alinhada com seu propósito, cada palavra conta.</p>
+					</div><!--box-single-diferenciais-->
+
+					<div class="box-single-diferenciais">
+						<img src="images/icon3.png" />
+						<h3>Ambientes Mobile</h3>
+						<p>Garanta que toda sua comunicação esteja alinhada com seu propósito, cada palavra conta.</p>
+					</div><!--box-single-diferenciais-->
+
+				</div><!--icons-diferenciais-->
+
+			</div>
+
+	</section>
+
+	<section class="sobre-time">
+		<div class="center">
+			<div class="w50 time-descricao-2">
+				<h2>Um time experiente,<br/>comunicador e coeso.</h2>
+				<p>A Product Runt acredita que marcas fortes são construídas a partir de transformações importantes e positivas na vida dos consumidores. Exatamente como uma conversa que ganha forma, a ideia vira flâmula.
+				<br /><br/>
+				Comunicar bem, de forma coerente e clara não é algo fácil e estamos aqui para ajudar você e seu time. Você escolhe, remoto ou presencial, para seu cliente ou seu público interno, você determina como vamos te ajudar.</p>
+			</div><!--w50-->
+			<div class="w50 img-time">
+				<img src="images/time.png" />
+			</div>
+			<div class="clear"></div>
+		</div>
+	</section>
+
+	<section class="depoimentos">
+		<div class="center">
+			<h2>Depoimentos</h2>
+			<div class="depoimentos-box">
+				<div class="depoimento-single">
+					<p>"Aliqua quis ut duis eiusmod laboris aliquip elit consequat incididunt occaecat minim in consequat ut id culpa veniam. Ut mollit culpa laboris reprehenderit esse id anim anim quis in sed laborum irure mollit culpa."</p>
+					<p>Guilherme Grillo</p>
+					<img src="images/autor.jpg" />
+				</div><!--depoimento-single-->
+				<div class="depoimento-single">
+					<p>"Aliqua quis ut duis eiusmod laboris aliquip elit consequat incididunt occaecat minim in consequat ut id culpa veniam. Ut mollit culpa laboris reprehenderit esse id anim anim quis in sed laborum irure mollit culpa."</p>
+					<p>Guilherme Grillo</p>
+					<img src="images/autor.jpg" />
+				</div><!--depoimento-single-->
+				<div class="depoimento-single">
+					<p>"Aliqua quis ut duis eiusmod laboris aliquip elit consequat incididunt occaecat minim in consequat ut id culpa veniam. Ut mollit culpa laboris reprehenderit esse id anim anim quis in sed laborum irure mollit culpa."</p>
+					<p>Guilherme Grillo</p>
+					<img src="images/autor.jpg" />
+				</div><!--depoimento-single-->
+			</div><!--depoimentos-box-->
+		</div>
+	</section>
+
+
+	<footer>
+	<section class="metodologia">
+		<div class="center">
+			<h2>Conheça nossa Metodologia</h2>
+			<p>O que acha de fazermos o que mais gostamos de fazer? Conversar!<br />Entre em contato por e-mail ou telefone.</p>
+			<a href="/contato">Entrar Contato</a>
+		</div>
+	</section>
+		<div class="center">
+			<div class="col-footer">
+				<h2>Suporte</h2>
+				<a href="">Contato</a>
+				<a href="">FAQ</a>
+			</div>
+			<div class="col-footer">
+				<h2>Suporte</h2>
+				<a href="">Contato</a>
+				<a href="">FAQ</a>
+			</div>
+			<div class="col-footer">
+				<h2>Suporte</h2>
+				<a href="">Contato</a>
+				<a href="">FAQ</a>
+			</div>
+			<div style="width: 40%;text-align: right;" class="col-footer">
+				<img src="images/logo-footer.png" />
+			</div>
+		</div>
+	</footer>
+
+<script src="js/jquery.js"></script>
+<script src="js/slick.min.js"></script>
+<script type="text/javascript">
+	$('section.clientes-slider .slider-container').slick({
+		dots: true,
+		arrows:false,
+	    infinite: false,
+	    speed:1000,
+	    slidesToShow: 4,
+	    autoplay: true,
+	    centerMode:false,
+	    autoplaySpeed: 3000,
+	    pauseOnHover:false,
+	    responsive: 
+	    [
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        slidesToShow: 2
+	      }
+	    }
+	    ]
+	});
+
+	$('section.depoimentos .depoimentos-box').slick({
+		dots: true,
+		arrows:false,
+	    infinite: true,
+	    speed:1000,
+	    slidesToShow: 1,
+	    centerMode:false
+	});
+</script>
+<script>
+	//Menu responsivo.
+	$('.menu-mobile i').click(function(){
+		$('.menu-mobile').find('ul').slideToggle();
+	})
+</script>
+</body>
+</html>
---
+++ sobre.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Danki Code - Tema</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+	<link href="style.css" rel="stylesheet">
+</head>
+<body>
+
+	<section class="topo">
+		<div class="center">
+			<header>
+				<div class="logo"><img src="images/logo.png" /></div><!--logo-->
+			</header>
+			<ul class="menu-desktop">
+				<li><a href="">Home</a></li>
+				<li><a href="">Sobre</a></li>
+				<li><a href="">Contato</a></li>
+			</ul>
+			<div class="menu-mobile">
+			<i class="fas fa-align-right"></i>
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Sobre</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+			</div><!--menu-mobile-->
+			<div class="clear"></div>
+		</div><!--center-->
+	</section><!--topo-->
+
+	<section class="sobre-equipe">
+	<div class="center">
+		<div class="w50 equipe-sobre-texto">
+			<h2>Uma equipe estratégica para um trabalho incrível</h2>
+			<p>Com foco permanente na geração de resultados para empresas, atuamos na consultoria estratégica e na criação e implementação de soluções de comunicação criativas, consistentes e adequadas às necessidades e características de cada companhia. Para isso, trabalhamos sempre em parceria e colaboração com interfaces e executivos das organizações, mantendo completo envolvimento com os negócios de cada cliente.</p>
+		</div>
+		<div class="w50">
+			<img src="images/foto-equipe.jpg" />
+		</div>
+		<div class="clear"></div>
+	</div>
+	</section>
+
+	<section class="sobre-imagens">
+		<div class="center">
+			<div class="img-sobre">
+				<img src="images/imagem-sobre.jpg" />
+			</div>
+			<div class="img-sobre">
+				<img src="images/imagem-sobre.jpg" />
+			</div>
+			<div class="img-sobre">
+				<img src="images/imagem-sobre.jpg" />
+			</div>
+			<div class="img-sobre">
+				<img src="images/imagem-sobre.jpg" />
+			</div>
+		</div>
+	</section>
+
+	<footer>
+	<section class="metodologia">
+		<div class="center">
+			<h2>Conheça nossa Metodologia</h2>
+			<p>O que acha de fazermos o que mais gostamos de fazer? Conversar!<br />Entre em contato por e-mail ou telefone.</p>
+			<a href="/contato">Entrar Contato</a>
+		</div>
+	</section>
+		<div class="center">
+			<div class="col-footer">
+				<h2>Suporte</h2>
+				<a href="">Contato</a>
+				<a href="">FAQ</a>
+			</div>
+			<div class="col-footer">
+				<h2>Suporte</h2>
+				<a href="">Contato</a>
+				<a href="">FAQ</a>
+			</div>
+			<div class="col-footer">
+				<h2>Suporte</h2>
+				<a href="">Contato</a>
+				<a href="">FAQ</a>
+			</div>
+			<div style="width: 40%;text-align: right;" class="col-footer">
+				<img src="images/logo-footer.png" />
+			</div>
+		</div>
+	</footer>
+</body>
+
+</html>
